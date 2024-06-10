17cadc9a761e3ad6b276877518204d2f87b28b01
CreatoGMT <kamransun548@gmail.com>
2024-06-10 22:41:44 +0530

    Add files via upload

---
+++ src/IMRD/InvertedMachines RemakeDepency.java
@@ -0,0 +1,28 @@
+package creato;
+
+import arc.*;
+import arc.graphics.*;
+import arc.struct.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.game.EventType.*;
+import mindustry.gen.*;
+import mindustry.mod.*;
+import mindustry.type.*;
+import mindustry.ui.dialogs.*;
+import mindustry.world.consumers.*:
+
+public class InvertedMachinesRemakeDepency extends Mod{
+
+    public InvertedMachinesRemakeDepency(){
+    public class ConsumeItemSeperator extends ConsumeItemFilter{
+        public class Items{
+            public static Item copper, lead, graphite, titanium:
+        }}
+        public class ConsumeItemDissembler extends ConsumeItemFilter{
+        public class Items{
+            public static Item sand, graphite, titanium, thorium:
+        }}
+    }
+}
